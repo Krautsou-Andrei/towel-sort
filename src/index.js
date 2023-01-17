a4036554c5cf7f06a16803da5d970e2ff3f25963
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2023-01-17 21:50:42 +0300

    modified:   package-lock.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    let array = [];
+
+    matrix.forEach((element, index) => {
+        if (index % 2 === 0) {
+            array.push(...element);
+        } else {
+            element.reverse();
+            array.push(...element);
+        }
+    });
+
+    return array;
+};
